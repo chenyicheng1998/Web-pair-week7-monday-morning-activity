34bb5641483f1820148627a8dad212a5087a6a71
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-29 11:41:00 +0300

    finish Iteration 3: Fetch and Delete a Single Job

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages & components
+import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage";
 import AddJobPage from "./pages/AddJobPage";
-import Navbar from "./components/Navbar";
+import JobPage from "./pages/JobPage"
 import NotFoundPage from "./pages/NotFoundPage"
 
 const App = () => {
 
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/add-job" element={<AddJobPage />} />
-              <Route path='*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/jobs/add-job" element={<AddJobPage />} />
+            <Route path="/jobs/:id" element={<JobPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
